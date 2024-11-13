65ae80a0763abe1200bf23400f9bf98b971c65a1
pachadomenic <pachadomenic2010@gmail.com>
2024-11-13 23:45:02 +0200

    sm8250-common: Update blobs to pipa OS1.0.12.0.UMZMIXM
    Change-Id: I51ded05b7f0e9d00514181a600dd12366b437dcd

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -182,7 +182,7 @@ SOONG_CONFIG_XIAOMI_KONA_PARTITION_SCHEME := a
 endif
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2024-02-01
+VENDOR_SECURITY_PATCH := 2024-11-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
