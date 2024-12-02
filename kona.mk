a36af26c70195c8ee4a8c7bd49851243a307ea20
Flicker372 <flicker372@outlook.com>
2024-12-02 19:57:11 +0800

    Revert "sm8250-common: remove OMX"
    This reverts commit 931a013de75d9aea4e82e85de01a7920c9810caf.

--- kona.mk
+++ kona.mk
@@ -251,6 +251,8 @@ $(call soong_config_set_bool,lineage_health,charging_control_supports_bypass,fal
 PRODUCT_PACKAGES += \
     media_codecs_c2.xml \
     media_codecs_performance_c2.xml \
+    media_codecs_kona.xml \
+    media_codecs_performance_kona.xml \
     video_system_specs.json
 
 ifneq ($(TARGET_IS_TABLET),true)
@@ -272,7 +274,16 @@ PRODUCT_COPY_FILES += \
 endif
 
 # OMX
-TARGET_SUPPORTS_OMX_SERVICE := false
+PRODUCT_PACKAGES += \
+    libOmxAacEnc \
+    libOmxAmrEnc \
+    libOmxCore \
+    libOmxEvrcEnc \
+    libOmxG711Enc \
+    libOmxQcelp13Enc \
+    libOmxVdec \
+    libOmxVenc \
+    libstagefrighthw
 
 # Overlays
 PRODUCT_PACKAGES += \
