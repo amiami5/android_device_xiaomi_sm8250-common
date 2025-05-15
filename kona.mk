5b278d6ee55e137004eae93f7bed556761eba5d7
CuriousNom <aryanbiswas7585@gmail.com>
2025-05-15 11:58:59 +0530

    sm8250-common: Suppress few logspams

--- kona.mk
+++ kona.mk
@@ -250,17 +250,27 @@ PRODUCT_PACKAGES += \
 
 # Logging
 SPAMMY_LOG_TAGS := \
+     AsyncOperation \
+     BackgroundInstallControlService \
+     BinderNative \
+     dxwi \
+     HWUI \
      MiStcImpl \
+     OtherActivityInputConsumer \
      SDM \
      SDM-histogram \
      SensorService \
      SRE \
+     WindowManager \
+     WindowManagerShell \
      WifiHAL \
      cnss-daemon \
      libcitsensorservice@2.0-impl \
      libsensor-displayalgo \
      libsensor-parseRGB \
      libsensor-ssccalapi \
+     libjingle \
+     StrictMode \
      sensors \
      TextView \
      PerformanceBooster \
