51a3b1020ff62336c4c04103f469efc548370596
mikeNG <mikeNG@users.noreply.github.com>
2025-05-25 14:15:31 +0000

    sm8250-common: Move Lineage Health HAL to select()
    * vendor/lineage/config/BoardConfigSoong.mk:50: warning: TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS is deprecated, please migrate to soong_config_set,lineage_health,charging_control_supports_bypass.

--- kona.mk
+++ kona.mk
@@ -258,6 +258,8 @@ endif
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
+$(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
+
 # Logging
 SPAMMY_LOG_TAGS := \
      AsyncOperation \
