a1b3234c9bf229ec4da8985c6e96f1558a0da8d2
Null Pointer <nullpointer1101@outlook.com>
2026-05-03 16:35:50 +0530

    sm8250-common: Set production ART and VNDK flags
    - Disable ART debug builds and minimize Java debug info to reduce system image size.
    - Enable VNDK core variant usage to prevent library duplication between partitions.
    
    Co-authored-by: Mathieu Chartier <mathieuc@google.com>
    Co-authored-by: Albert I <kras@raphielgang.org>

--- kona.mk
+++ kona.mk
@@ -157,6 +157,17 @@ endif
 PRODUCT_PACKAGES += \
     XiaomiParts
 
+# Do not generate libartd.
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+
+# Strip the local variable table and the local variable type table to reduce
+# the size of the system image. This has no bearing on stack traces, but will
+# leave less information available via JDWP.
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+
+# Dedupe VNDK libraries with identical core variants.
+TARGET_VNDK_USE_CORE_VARIANT := true
+
 # Display
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.composer-service
