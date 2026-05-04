ba22453e707c7be3bcd0dcedf2aba326b0aa433e
Null Pointer <nullpointer1101@outlook.com>
2026-05-04 11:59:09 +0530

    sm8250-common: Unlock Wi-Fi 6 (11AX) Hotspot capabilities
    - Enable CONFIG_ACS to allow automatic selection of the cleanest channel.
    - Enable CONFIG_IEEE80211AC and CONFIG_IEEE80211AX to unlock the
      5GHz band and 802.11ax modulation at the driver level.
    - Set WIFI_FEATURE_HOSTAPD_11AX to true to ensure the hostapd service
      is compiled with support for Wi-Fi 6 broadcasting.
    
    Co-Authored-By: Peng Xu <pxu@qca.qualcomm.com>
    Co-Authored-By: zenzer0s <praveenzalaki200@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -245,5 +245,11 @@ WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
 WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
+# Wi-Fi Hotspot
+CONFIG_ACS := true
+CONFIG_IEEE80211AC := true
+CONFIG_IEEE80211AX := true
+WIFI_FEATURE_HOSTAPD_11AX := true
+
 # Inherit the proprietary files
 include vendor/xiaomi/sm8250-common/BoardConfigVendor.mk
